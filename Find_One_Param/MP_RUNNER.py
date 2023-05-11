01e3185fa6fac337e38b04e40c79f4ea6f5390d6
Gal Levy <gallevy88@gmail.com>
2023-05-11 13:44:25 +0300

    Universal MP + Analysis MP

--- Find_One_Param/MP_RUNNER.py
+++ Find_One_Param/MP_RUNNER.py
@@ -1,40 +1,36 @@
 import multiprocessing
 import os
-import psutil
 import subprocess
 import warnings
+
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
+
 def worker(script_path, args):
-    # Get the process ID and CPU core number of the current worker
+    # Get the process ID
     pid = os.getpid()
-    core = psutil.Process(pid).cpu_affinity()[0]
 
     # Execute the Python script as a separate process with command-line arguments
     cmd = ['python', script_path] + args
     subprocess.run(cmd)
 
-    # Return the process ID and CPU core number
-    return (pid, core)
+    # Return the process ID
+    return pid
+
 
 if __name__ == '__main__':
     # Determine the number of CPU cores to use
     num_cores = multiprocessing.cpu_count()
-    print(f"Num Of Cores {num_cores}")
+    print(f"Num Of Cores: {num_cores}")
 
     # Create a list of tasks, one for each CPU core
     tasks = []
-    for i in range(100):
+    for i in range(300):
         task = {'script_path': 'app_mp.py', 'args': ['--task', str(i)]}
         tasks.append(task)
 
     # Create a pool of workers
     pool = multiprocessing.Pool(processes=num_cores)
-
-    # Set the CPU affinity for each worker process
-    for i, worker_process in enumerate(pool._pool):
-        core = i % num_cores
-        psutil.Process(worker_process.pid).cpu_affinity([core])
 
     # Assign a task and script path with arguments to each worker
     results = []
@@ -46,5 +42,5 @@ if __name__ == '__main__':
 
     # Wait for all tasks to complete and get the results
     for result in results:
-        pid, core = result.get()
-        print(f"Worker with PID {pid} on CPU core {core} executed a Python script Succesfully!")
+        pid = result.get()
+        print(f"Worker with PID {pid} executed a Python script successfully!")
